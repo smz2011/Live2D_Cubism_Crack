208bb10621b755494b5bceec1355f365e7f26855
Bai <a1640727878@163.com>
2023-06-06 19:59:47 +0800

    updata

---
+++ Install 一键安装.bat
@@ -0,0 +1,11 @@
+set JAVA_EXE=app\jre\bin\java.exe
+
+:Admin
+
+%JAVA_EXE% ^
+  -classpath "Live2D_Cubism_Crack_5-5.0.jar" ^
+  -showversion ^
+  com.live2d.cubism.CrackCubismEditorMain ^
+  "%~f1"
+  
+pause
